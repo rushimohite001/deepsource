f18e3d165b541f04f32c698116ea9150b7afc57e
rushimohite001 <rushimohite2002@gmail.com>
2025-04-21 22:41:26 +0530

    Add test resource to trigger DeepSource in PR

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ provider "azurerm" {
   subscription_id = "1ac2caa4-336e-4daa-b8f1-0fbabe2d4b11"
 }
 
-resource "azurerm_resource_group" "demo_rg" {
-  name     = var.resource_group_name
-  location = var.location
+resource "azurerm_resource_group" "test" {
+  name     = "hardcoded-name"   # <-- this will trigger a warning
+  location = "eastus"
 }
 
